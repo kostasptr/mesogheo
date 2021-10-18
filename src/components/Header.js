a8b579398653369c0dedbefb3afc76c3166fa6ea
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-18 12:27:18 +0300

    header for mobile added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import logo from '../images/logo_mesogheo.jpg';
 import './Header.css';
 
 function Header() {
 
   return (
-    <div className="App-Header">
-    <h1>Header</h1>
-    </div>
+    <header className="App-Header">
+      <nav className="flex-nav">
+        <div>
+          <a href="/">
+            <img src={logo} alt="logo Mesogheo"></img>
+          </a>
+        </div>
+        <div>
+          <ul className="nav-items">
+            <li className="nav-link"><Link to="rooms"> ROOMS </Link></li>
+            <li className="nav-link"><Link to="about"> ABOUT </Link></li>
+            <li className="nav-link"><Link to="book"> BOOK </Link></li>
+          </ul>
+        </div>
+      
+      </nav>
+    </header>
   );
 }
 export default Header;
