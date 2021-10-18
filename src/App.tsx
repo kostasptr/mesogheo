8d91418c0feddc236f54c92158aca98f7a40e19a
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-18 10:41:31 +0300

    navigation created

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Routing, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+import Rooms from './components/Rooms';
+import About from './components/About';
+import Book from './components/Book';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Routing>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/mesgheo" component={Home} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/rooms" component={Rooms} />
+          <Route path="/about" component={About} />
+          <Route path="/book" component={Book} />
+        </Switch>
+        <Footer />
+      </div>
+    </Routing>
+
   );
 }
 
