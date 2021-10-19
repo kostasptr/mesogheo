e3268f8acf68c3113ed75d2c7aa27ace21eee3ba
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-19 16:03:36 +0300

    hero image added for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
-// import mesogheoPool from '../images/mesogheoPool.png';
+import mesogheoPool from '../images/hero_image.jpg';
 
 function Home() {
 
   return (
-    <div className="grid grid-cols-12 px-1 gap-x-2 bg-92_green">
-      {/* <img src={mesogheoPool} alt="mesogheo hotel" className="pb-4"/> */}
-      <div className="col-start-1 col-end-12 pb-6 font-serif text-xs not-italic font-normal leading-2b tracking-normal text-left text-22_green">
-      <p>Mesogheo is a bed & breakfast with 4 rooms, each of them carefully decorated in an elegant minimalistic style that contrasts the maximalism of the outside area. The evergreen forest surrounds the property in a way that makes you forget you are at a few kilometers from bustling Naples.</p>
+    <div className="grid grid-cols-12 gap-x-2 bg-92_green">
+      <img src={mesogheoPool} alt="mesogheo hotel" className="block col-span-12 w-screen pb-4"/>
+      <div className="col-start-2 col-end-12 pb-4 px-1 font-serif text-xs not-italic font-normal leading-2b tracking-normal text-left text-small12 text-22_green">
+      <p  className="mb-2">Mesogheo is a bed & breakfast with 4 rooms, each of them carefully decorated in an elegant minimalistic style that contrasts the maximalism of the outside area.</p><p>The evergreen forest surrounds the property in a way that makes you forget you are at a few kilometers from bustling Naples.</p>
       </div>
     </div>
   );
