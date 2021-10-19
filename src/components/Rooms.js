2490f78d01be7c6da6c1d1821e7b2a09ab97cac2
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-19 20:14:32 +0300

    commented out

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,19 +1,20 @@
-import React from 'react';
+// import React from 'react';
 
-class Rooms extends React.Component {
+// class Rooms extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.myRef = React.createRef();
-  }
+//   constructor(props) {
+//     super(props);
+//     this.myRef = React.createRef();
+//   }
 
-  render() {
-    return (
-      <div className="App-Rooms" ref={this.myRef}>
-      <p>Room 'Arizona' is our cozy nest, has a large bathroom and a private patio where you can enjoy your meals under the warm south Italian sun.</p>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="grid grid-cols-12 gap-x-2 bg-92_green" ref={this.myRef}>
+      
+//       <p className="col-start-3 col-end-11 block font-serif text-small font-normal not-italic leading-smallf  tracking-normal  text-center text-92_green pt-4 pb-2">Room 'Arizona' is our cozy nest, has a large bathroom and a private patio where you can enjoy your meals under the warm south Italian sun.</p>
+//       </div>
+//     );
+//   }
+// }
 
-export default Rooms;
+// export default Rooms;
