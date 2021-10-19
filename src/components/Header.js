8a27351a4e6ff028e18479a2e9312a5bdc650a28
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-19 17:03:32 +0300

    link to Rooms added

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,10 +14,10 @@ function Header() {
         </div>
         <div className="col-start-3 col-end-11 flex justify-between pb-4">
           {/* <ul> */}
-            <li className="inline font-serif text-base not-italic font-normal leading-5 tracking-normal text-center text-22_green">
-            <Link to="rooms"> ROOMS </Link></li>
-            <li className="inline font-serif  text-base  not-italic font-normal leading-5 tracking-normal text-center text-22_green"><Link to="about"> ABOUT </Link></li>
-            <li className="inline font-serif  text-base  not-italic font-normal leading-5 tracking-normal text-center text-pink"><Link to="book"> BOOK </Link></li>
+            <li className="inline font-serif text-base not-italic font-normal leading-5 tracking-normal text-center text-22_green active:line-through">
+            <Link to="this.myRef"> ROOMS </Link></li>
+            <li className="inline font-serif  text-base  not-italic font-normal leading-5 tracking-normal text-center text-22_green active:line-through"><Link to="about"> ABOUT </Link></li>
+            <li className="inline font-serif  text-base  not-italic font-normal leading-5 tracking-normal text-center text-pink active:line-through"><Link to="book"> BOOK </Link></li>
           {/* </ul> */}
         </div>
       
