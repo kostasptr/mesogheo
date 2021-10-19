ca7b55251820a9d54fb2c6738dd05ab7e59cafc9
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-19 20:13:15 +0300

    rooms component disabled

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Routing, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/Header';
-import Rooms from './components/Rooms';
+// import Rooms from './components/Rooms';
 import About from './components/About';
 import Book from './components/Book';
 import Footer from './components/Footer';
@@ -19,7 +19,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/mesogheo" component={Home} />
           <Route path="/home" exact component={Home} />
-          <Route path="/rooms" component={Rooms} />
+          {/* <Route path="/rooms" component={Rooms} /> */}
           <Route path="/about" component={About} />
           <Route path="/book" component={Book} />
           <Route path="/faq" component={FAQ} />
