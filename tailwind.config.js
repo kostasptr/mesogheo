8edbb4b0dda8656aa0fb108df4887d8400fc605b
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-19 11:16:28 +0300

    configuration par. added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,43 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens:{
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '3b': '20px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
+    colors: {
+      '22_green': '#39412F',
+      'pink': '#FF4365',
+      '92_green': '#EBEEE7',
+    },
+    fontFamily: {
+      'sans': ['Graphik', 'sans-serif'],
+      'serif': ['Lora', 'serif'],
+    },
+    fontSize: {
+      'small' : '.625rem',
+    },
+    lineHeight: {
+      'smallf': '.823rem',
+    }
   },
   variants: {
     extend: {},
