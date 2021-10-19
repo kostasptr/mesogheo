788118002230103e419325bb9d2205abc8712481
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-19 20:13:52 +0300

    hover class added

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,9 +14,9 @@ function Header() {
         </div>
         <div className="col-start-3 col-end-11 flex justify-between pb-4">
           {/* <ul> */}
-            <li className="inline font-serif text-base not-italic font-normal leading-5 tracking-normal text-center text-22_green active:line-through">
-            <Link to="this.myRef"> ROOMS </Link></li>
-            <li className="inline font-serif  text-base  not-italic font-normal leading-5 tracking-normal text-center text-22_green active:line-through"><Link to="about"> ABOUT </Link></li>
+            <li className="inline font-serif text-base not-italic font-normal leading-5 tracking-normal text-center text-22_green hover:text-pink active:line-through">
+            <Link to="room"> ROOMS </Link></li>
+            <li className="inline font-serif  text-base  not-italic font-normal leading-5 tracking-normal text-center text-22_green  hover:text-pink  active:line-through"><Link to="about"> ABOUT </Link></li>
             <li className="inline font-serif  text-base  not-italic font-normal leading-5 tracking-normal text-center text-pink active:line-through"><Link to="book"> BOOK </Link></li>
           {/* </ul> */}
         </div>
