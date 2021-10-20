28b780369c91937ada38566c232c8f907a9de3cc
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-20 21:41:01 +0300

    change class to child

--- src/components/About.js
+++ src/components/About.js
@@ -4,17 +4,17 @@ function About() {
 
   return (
     <div className="grid grid-cols-12 px-1 gap-x-2 bg-92_green pb-4c">
-		<div className="col-start-2 col-end-12 font-sans not-italic leading-4 text-left font-light text-small12 text-02_green tracking-normal">
+		<div className="col-start-2 col-end-12 font-sans not-italic leading-4 text-left font-light text-small12  tracking-normal">
 			<p className="font-normal text-22_green">About:</p>
-			<p>Our goal at Mesogheo is to provide you an authentic experience of fresh tradition.
+			<p className="text-02_green">Our goal at Mesogheo is to provide you an authentic experience of fresh tradition.
 			Simplicity meets nature in its wildest form in order to best serve the need of getting away and being inspired.
 			All spaces are furnished with superior quality items that keep a generous yet essential vibe.</p><br></br>
 			<p className="font-normal text-22_green">Neighbourhood information:</p>
-			<p>Mesogheo is located just outside Melizzano, a small village situated at the feet of mount Taburno.
+			<p className="text-02_green">Mesogheo is located just outside Melizzano, a small village situated at the feet of mount Taburno.
 			The area is well known for the finest wine varietals and lately in the neighbourhood have shown up Michelin awarded restaurants.
 			When it comes to cultural and gastronomic turism our special place is a safe choice.</p><br></br>
 			<p className="font-normal text-22_green">Languages spoken:</p>
-			<p>Italian, English, French, Greek</p>
+			<p className="text-02_green">Italian, English, French, Greek</p>
 		</div>
     </div>
   );
