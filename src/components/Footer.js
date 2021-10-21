243af340193722febea6f9df06f36d28306061ef
kostasptr <kostas-ptr@hotmail.com>
2021-10-21 16:54:35 +0300

    icons' width changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Facebook, Instagram } from 'react-ionicons';
 import { Link } from "react-router-dom";
 import facebookIcon from "../images/logo-facebook.svg";
 import instagramIcon from "../images/logo-instagram 1.svg";
@@ -12,7 +11,7 @@ function Footer() {
 
       <div className="col-start-3 col-end-11 block font-serif text-small font-normal not-italic leading-4  tracking-normal  text-center text-92_green pt-4 pb-2">
       <a href="https://www.google.com/maps/place/Mesogheo/@41.167586,14.4935449,17z/data=!4m13!1m7!3m6!1s0x133a45678f7637d9:0xf26876cd24a547e0!2zVmlhIFZhbGxlIENvcnJhZG8sIDQwLCA4MjAzMCBNZWxpenphbm8gQk4sIM6Zz4TOsc67zq_OsQ!3b1!8m2!3d41.1659949!4d14.4961413!3m4!1s0x133a45678fb976d1:0xa29b46e8794df151!8m2!3d41.1684235!4d14.4943019" target="_blank" rel="noopener noreferrer">
-          <p className="hover:text-pink">Valle Corrado 40, 82030 Melizzano, Campania, Italy</p>
+          <p className="hover:text-pink">valle Corrado 40, 82030 Melizzano, Campania, Italy</p>
         </a>
         <a href="tel:+39 3887842886" target="_blank" rel="noopener noreferrer">
           <p className="hover:text-pink">phone number: +39 3887842886</p>
@@ -25,14 +24,12 @@ function Footer() {
         </Link>
       </div>
 
-      <div className="col-start-6 col-end-8 flex flex-row justify-evenly inline pb-3b">
+      <div className="col-start-6 col-end-8 flex flex-row justify-between inline pb-3a">
         <a href="https://www.instagram.com/mesogheo/?hl=en" target="_blank" rel="noopener noreferrer">
-          {/* <Instagram color={'#FFFFFF'} /> */}
-          <img src={instagramIcon} alt="instagram icon" className="w-3b "/>
+          <img src={instagramIcon} alt="instagram icon" className="w-3a"/>
         </a>
         <a href="https://www.facebook.com/Mesogheo" target="_blank" rel="noopener noreferrer">
-          <img src={facebookIcon} alt="facebook icon" className="w-3b ml-1"/>
-          {/* <Facebook color={'#FFFFFF'} /> */}
+          <img src={facebookIcon} alt="facebook icon" className="w-3a"/>
         </a>
       </div>
 
