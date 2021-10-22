9d18c85c5aa7e8cd2383c3434a8038e1b5638d22
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-23 00:49:48 +0200

    No need for key in KeenSliderDot

--- src/components/KeenSliderDot.js
+++ src/components/KeenSliderDot.js
@@ -3,14 +3,13 @@ import { useState } from "react";
 import {ReactComponent as DotActive} from "../images/carousel-navigation/dot-active.svg";
 import {ReactComponent as Dot} from "../images/carousel-navigation/dot.svg";
 
-const KeenSliderDot = ({ active, key, onClick }) => {
+const KeenSliderDot = ({ active, onClick }) => {
   const [isHovered, setHovered] = useState(false);
   const textColor = isHovered ? "text-pink" : "text-22_green";
   const toggleHover = () => setHovered(!isHovered);
 
   return active ? (
     <DotActive
-      key={key}
       className={`cursor-pointer ${textColor}`}
       onClick={onClick}
       onMouseEnter={toggleHover}
@@ -18,7 +17,6 @@ const KeenSliderDot = ({ active, key, onClick }) => {
     />
   ) : (
     <Dot
-      key={key}
       className={`cursor-pointer ${textColor}`}
       onClick={onClick}
       onMouseEnter={toggleHover}
