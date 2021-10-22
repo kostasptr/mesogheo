8b9382396af62245883d44d47c7a2e0b87a94775
kostasptr <kostas-ptr@hotmail.com>
2021-10-22 19:03:24 +0300

    imported keenslider amenities and roomlinks

--- src/components/rooms/arizona.js
+++ src/components/rooms/arizona.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import KeenSlider from '../KeenSlider';
-import airconditioner from '../../images/amenities/air_conditioner.svg';
-import breakfast from '../../images/amenities/breakfast.svg';
-import hair_dryer from '../../images/amenities/hair_dryer.svg';
-import safe from '../../images/amenities/safe.svg';
-import shower from '../../images/amenities/shower.svg';
-import tv from '../../images/amenities/tv.svg';
-import wifi from '../../images/amenities/wifi.svg';
+import Roomlinks from "./Roomlinks";
+import Amenities from "./Amenities";
 import arizona_1 from '../../images/Arizona/arizona_1.jpg';
 import arizona_2 from '../../images/Arizona/arizona_2.jpg';
 import arizona_3 from '../../images/Arizona/arizona_3.jpg';
@@ -33,42 +27,17 @@ function Arizona() {
 
   return (
     <>
-
     <KeenSlider images={images} />
     <div className="grid grid-cols-12 gap-x-2 px-1 mb-5 pt-3b">
 
-      <div className="col-start-4 col-end-10 mb-1 mt-3 font-serif not-italic font-normal  flex  justify-around">
-        <Link to="/arizona" >
-          <p className="text-22_green text-small12 leading-4 hover:text-pink active:line-through">ARIZONA</p>
-        </Link>
-        <Link to="/viola" className="text-small12 text-22_green hover:text-pink active:line-through">
-          <p>VIOLA</p>
-        </Link>
-      </div>
-
-      <div className="col-start-4 col-end-10 mb-2 font-serif not-italic font-normal leading-4  flex  justify-around">
-        <Link to="/verde" className="text-small12 text-22_green hover:text-pink active:line-through pl-1">
-          <p>VERDE</p>
-        </Link>
-        <Link to="/blu" className="text-small12 text-22_green hover:text-pink active:line-through pr-1 ">
-          <p>BLU</p>
-        </Link>
-      </div>
+      <Roomlinks />
 
       <div className="col-start-2 col-end-12 font-sans not-italic leading-4 text-left font-light tracking-normal mb-3">
         <p className="text-small12 text-22_green">ARIZONA ROOM:</p>
         <p className="text-small12 text-22_green ">Room 'Arizona' is our cozy nest, has a large bathroom & a private patio where you can enjoy your meals under the warm south Italian sun. </p>
       </div>
 
-      <div className="col-start-2 col-end-9 flex flex-row justify-between">
-        <img src={airconditioner} alt="airconditioner amenities' icon"/>
-        <img src={breakfast} alt="breakfast amenities' icon"/>
-        <img src={hair_dryer} alt="hair_dryer amenities' icon"/>
-        <img src={safe} alt="safe amenities' icon"/>
-        <img src={shower} alt="shower amenities' icon"/>
-        <img src={tv} alt="tv amenities' icon"/>
-        <img src={wifi} alt="wifi amenities' icon"/>
-      </div>
+      <Amenities className="col-start-2 col-end-9 flex flex-row justify-between" />
 
     </div>
     </>
