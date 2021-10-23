4c8a88e98ad9f6c5856bd3327c365734c9a17a37
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-23 04:53:36 +0200

    Amenities from imgs to svgs

--- src/components/rooms/Amenities.js
+++ src/components/rooms/Amenities.js
@@ -1,21 +1,21 @@
-import airconditioner from '../../images/amenities/air_conditioner.svg';
-import breakfast from '../../images/amenities/breakfast.svg';
-import hair_dryer from '../../images/amenities/hair_dryer.svg';
-import safe from '../../images/amenities/safe.svg';
-import shower from '../../images/amenities/shower.svg';
-import tv from '../../images/amenities/tv.svg';
-import wifi from '../../images/amenities/wifi.svg';
+import {ReactComponent as AirConditioner} from '../../images/amenities/air_conditioner.svg';
+import {ReactComponent as Breakfast} from '../../images/amenities/breakfast.svg';
+import {ReactComponent as HairDryer} from '../../images/amenities/hair_dryer.svg';
+import {ReactComponent as Safe} from '../../images/amenities/safe.svg';
+import {ReactComponent as Shower} from '../../images/amenities/shower.svg';
+import {ReactComponent as TV} from '../../images/amenities/tv.svg';
+import {ReactComponent as WiFi} from '../../images/amenities/wifi.svg';
 
 const Amenities = () => {
   return (
-    <div className="col-start-2 col-end-9 flex flex-row justify-between">
-      <img src={airconditioner} alt="airconditioner amenities' icon"/>
-      <img src={breakfast} alt="breakfast amenities' icon"/>
-      <img src={hair_dryer} alt="hair_dryer amenities' icon"/>
-      <img src={safe} alt="safe amenities' icon"/>
-      <img src={shower} alt="shower amenities' icon"/>
-      <img src={tv} alt="tv amenities' icon"/>
-      <img src={wifi} alt="wifi amenities' icon"/>
+    <div className="col-start-2 col-end-9 flex flex-row justify-between space-x-2">
+      <WiFi />
+      <Breakfast />
+      <AirConditioner />
+      <TV />
+      <Shower />
+      <HairDryer />
+      <Safe />
     </div>
   );
 };
