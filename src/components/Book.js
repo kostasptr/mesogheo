cdfd02971762782412f010fe871b914ee5aa5303
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-24 04:11:56 +0200

    not pink FAQ in Book page & regular pricelist

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,7 +6,7 @@ const Book = () => {
 			<div className="col-start-2 col-end-12 font-sans not-italic leading-4 text-left font-light text-small12 text-02_green tracking-normal">
 				<p className="mb-3">For information & booking you can contact us on:</p>
 				<p className="mb-5"><a href="tel:+393887842886" target="_blank" rel="noopener noreferrer">WhatsApp: +39 3887842886</a><br></br><a href="tel:+393887842886" target="_blank" rel="noopener noreferrer">Phone number: +39 3887842886</a><br></br><a href="mailto: mesogheo@googlemail.com" target="_blank" rel="noopener noreferrer">E-mail: mesogheo@googlemail.com</a></p>
-				<p className="mb-3">Pricelist:</p>
+				<p className="mb-3 font-normal">Pricelist:</p>
 				<p className="mb-3">Loft Viola & Loft Verde<br></br>
 				Sunday-Thursday: 120€ / Friday-Saturday: 150€</p>
 				<p className="mb-3">Arizona Room<br></br>
@@ -15,7 +15,7 @@ const Book = () => {
 				Sunday-Thursday: 110€ / Friday-Saturday: 130€</p>
 				<p className="mb-3">*Breakfast included in your booking</p>
 				<p className="mb-4 ">*Check in from 15.00 to 20.00<br></br>*Check out until 12.00</p>
-				<p className="text-center italic">for more information visit our <span className="text-pink">FAQ</span> page</p>
+				<p className="text-center italic">for more information visit our FAQ page</p>
 			</div>
 		</div>
 	);
