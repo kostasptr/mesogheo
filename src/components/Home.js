18ff840388a77fe3df1ac08228dd11871cffd903
kostasptr <kostas-ptr@hotmail.com>
2021-10-24 20:23:43 +0300

    md class added

--- src/components/Home.js
+++ src/components/Home.js
@@ -35,8 +35,8 @@ const Home = () => {
     <>
       <img src={mesogheoPool} alt="mesogheo hotel" className="col-span-full w-screen mb-2"/>
 
-      <div className="grid grid-cols-12 gap-x-2 px-1">
-        <div className="col-start-2 col-end-12 mb-4 font-sans text-small12 not-italic font-light leading-4 text-left text-02_green">
+      <div className="grid grid-cols-12 gap-x-2 px-1 md:mx-7 md:px-0 md:gap-x-2">
+        <div className="col-start-2 col-end-12 mb-4 font-sans text-small12 not-italic font-light leading-4 text-left text-02_green md:text-lg md:leading-7 md:mt-3b md:mb-5b ">
           <p className="mb-2">At the feet of the mount Taburno, in its national park area a magical place has opened its doors. The perfect destination for nature lovers and wanderlust travelers who seek alternative luxury.</p>
           <p className="mb-2">Mesogheo is a bed & breakfast with 4 rooms, each of them carefully decorated in an elegant minimalistic style that contrasts the maximalism of the outside area.</p>
           <p>The evergreen forest surrounds the property in a way that makes you forget you are at a few kilometers from bustling Naples.</p>
@@ -45,10 +45,10 @@ const Home = () => {
       
       <KeenSlider imagesArray={imagesArray}/>
 
-      <div className="grid grid-cols-12 gap-x-2 px-1">
+      <div className="grid grid-cols-12 gap-x-2 px-1 md:mx-7 md:px-0 md:gap-x-2">
         <Roomlinks />
 
-        <p className="col-start-2 col-end-12 mb-5 font-sans text-small12 not-italic font-light text-left text-02_green leading-4">
+        <p className="col-start-2 col-end-12 mb-5 font-sans text-small12 not-italic font-light text-left text-02_green leading-4 md:text-lg md:leading-7 md:mt-3b md:mb-5b">
           Our rooms are equipped with numerous amenities in order to ensure comfort & luxury. Cartel decoration, high quality double mattresses, homemade breakfast, an outdoor swimming pool for the summer, a fireplace for the winter & Netflix are among them.
         </p>
       </div>
--- src/components/rooms/Roomlinks.js
+++ src/components/rooms/Roomlinks.js
@@ -5,14 +5,14 @@ const RoomsLinks = () => {
 
   return (
     <>
-      <div className="col-start-5 col-end-9 mb-1 font-serif not-italic font-normal flex justify-between">
-        <Link to="/arizona" className={`text-small12 leading-4 text-22_green hover:text-pink ${pathname==='/arizona' ? "line-through" : ""}`} >ARIZONA</Link>
-        <Link to="/viola" className={`text-small12 text-22_green hover:text-pink ${pathname==='/viola' ? "line-through" : ""}`}>VIOLA</Link>
+      <div className="col-start-5 col-end-9 mb-1 font-serif not-italic font-normal flex justify-between md:mb-3b md:mt-3b">
+        <Link to="/arizona" className={`text-small12 leading-4 text-22_green hover:text-pink md:text-xl md:leading-7 ${pathname==='/arizona' ? "line-through" : ""}`} >ARIZONA</Link>
+        <Link to="/viola" className={`text-small12 text-22_green hover:text-pink md:text-xl md:leading-7 ${pathname==='/viola' ? "line-through" : ""}`}>VIOLA</Link>
       </div>
 
-      <div className="col-start-5 col-end-9 mb-2 font-serif not-italic font-normal leading-4 flex justify-between">
-        <Link to="/verde" className={`text-small12 text-22_green hover:text-pink ${pathname==='/verde' ? "line-through" : ""}`}>VERDE</Link>
-        <Link to="/blu" className={`text-small12 text-22_green hover:text-pink ${pathname==='/blu' ? "line-through" : ""}`}>BLU</Link>
+      <div className="col-start-5 col-end-9 mb-2 font-serif not-italic font-normal leading-4 flex justify-between mt-0 pt-0 md:mb-3b md:pt-0 ">
+        <Link to="/verde" className={`text-small12 text-22_green hover:text-pink md:text-xl md:leading-7 ${pathname==='/verde' ? "line-through" : ""}`}>VERDE</Link>
+        <Link to="/blu" className={`text-small12 text-22_green hover:text-pink md:text-xl md:leading-7 ${pathname==='/blu' ? "line-through" : ""}`}>BLU</Link>
       </div>
     </>
   );
