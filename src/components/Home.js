a3c43641be01c10779c376908e8e24b8d63c74a5
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-25 02:39:34 +0200

    Minor content changes in Landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -37,7 +37,7 @@ const Home = () => {
 
       <div className="grid grid-cols-12 gap-x-2 px-1 md:mx-7 md:px-0 md:gap-x-2">
         <div className="col-start-2 col-end-12 mb-4 font-sans text-small12 not-italic font-light leading-4 text-left text-02_green md:text-lg md:leading-7 md:mt-3b md:mb-5b ">
-          <p className="mb-2">At the feet of the mount Taburno, in its national park area a magical place has opened its doors. The perfect destination for nature lovers and wanderlust travelers who seek alternative luxury.</p>
+          <p className="mb-2">At the feet of the mount Taburno, in the national park area a magical place has opened its doors. The perfect destination for nature lovers and wanderlust travelers who seek alternative luxury.</p>
           <p className="mb-2">Mesogheo is a bed & breakfast with 4 rooms, each of them carefully decorated in an elegant minimalistic style that contrasts the maximalism of the outside area.</p>
           <p>The evergreen forest surrounds the property in a way that makes you forget you are at a few kilometers from bustling Naples.</p>
         </div>
@@ -49,7 +49,7 @@ const Home = () => {
         <Roomlinks />
 
         <p className="col-start-2 col-end-12 mb-5 font-sans text-small12 not-italic font-light text-left text-02_green leading-4 md:text-lg md:leading-7 md:mt-3b md:mb-5b">
-          Our rooms are equipped with numerous amenities in order to ensure comfort & luxury. Cartel decoration, high quality double mattresses, homemade breakfast, an outdoor swimming pool for the summer, a fireplace for the winter & Netflix are among them.
+          Our rooms are equipped with numerous amenities in order to ensure comfort & luxury. Kartell decoration, high quality double mattresses, homemade breakfast, an outdoor swimming pool for the summer, a fireplace for the winter & Netflix are among them.
         </p>
       </div>
     </>
