d8ca2e980c1c772be436af5f938930308160a08a
kostasptr <kostas-ptr@hotmail.com>
2021-10-25 11:27:47 +0300

    md class added

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="grid grid-cols-12 px-1 gap-x-2 pb-5">
-			<div className="col-start-2 col-end-12 font-sans not-italic leading-4 text-left font-light text-small12  tracking-normal">
-				<p className="font-normal text-22_green">About:</p>
-				<p className="text-02_green">Our goal at Mesogheo is to provide you an authentic experience of fresh tradition.
+    <div className="grid grid-cols-12 px-1 gap-x-2 pb-5 md:px-0 md:pb-0 md:mx-7 md:mb-194">
+			<div className="col-start-2 col-end-12 font-sans not-italic leading-4 text-left font-light text-small12 tracking-normal md:text-xl md:leading-5">
+				<p className="font-normal text-22_green ">About:</p>
+				<p className="text-02_green md:mb-3b ">Our goal at Mesogheo is to provide you an authentic experience of fresh tradition.
 				Simplicity meets nature in its wildest form in order to best serve the need of getting away and being inspired.
 				All spaces are furnished with superior quality items that keep a generous yet essential vibe.</p><br></br>
-				<p className="font-normal text-22_green">Neighbourhood information:</p>
-				<p className="text-02_green">Mesogheo is located just outside Melizzano, a small village situated at the feet of mount Taburno.
+				<p className="font-normal text-22_green ">Neighbourhood information:</p>
+				<p className="text-02_green md:mb-3b ">Mesogheo is located just outside Melizzano, a small village situated at the feet of mount Taburno.
 				The area is well known for the finest wine varietals and lately in the neighbourhood have shown up Michelin awarded restaurants.
 				When it comes to cultural and gastronomic turism our special place is a safe choice.</p><br></br>
-				<p className="font-normal text-22_green">Languages spoken:</p>
+				<p className="font-normal text-22_green ">Languages spoken:</p>
 				<p className="text-02_green">Italian, English, French, Greek</p>
 			</div>
     </div>
