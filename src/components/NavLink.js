e966565c3f5cbdcbf9e34019366e4345cba6f3e9
kostasptr <kostas-ptr@hotmail.com>
2021-10-26 10:52:54 +0300

    sm2 class added 1440px

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -10,7 +10,7 @@ const NavLink = ({link, hash=false, text, cta=false}) => {
   const pathname = useLocation().pathname;
 
   return (
-    <li className={`inline font-serif text-base not-italic font-normal leading-5 tracking-normal text-center md:text-2.5xl md:leading-height36 lg:text-base2 lg:leading-5c ${textColor} ${pathname===link ? "line-through" : ""}`} onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
+    <li className={`inline font-serif text-base not-italic font-normal leading-5 tracking-normal text-center md:text-2.5xl md:leading-height36 lg:text-base2 lg:leading-5c sm2:text-base2 sm2:leading-height41 sm2:font-normal ${textColor} ${pathname===link ? "line-through" : ""}`} onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
       {hash ? <HashLink smooth to={link}> {text} </HashLink> : <Link to={link}> {text} </Link>}
     </li>
   );
