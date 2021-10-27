42b242269e7a864e265b6cc18891a25616c58b6e
kostasptr <kostas-ptr@hotmail.com>
2021-10-27 17:18:25 +0300

    code commented out

--- src/components/rooms/Amenities.js
+++ src/components/rooms/Amenities.js
@@ -4,17 +4,17 @@ import {ReactComponent as AirConditioner} from '../../images/amenities/air_condi
 import {ReactComponent as TV} from '../../images/amenities/tv.svg';
 import {ReactComponent as Shower} from '../../images/amenities/shower.svg';
 import {ReactComponent as HairDryer} from '../../images/amenities/hair_dryer.svg';
-// import {ReactComponent as sqrmArizona} from '../../images/amenities/sqrmArizona.svg';
-// import {ReactComponent as sqrmViola} from '../../images/amenities/sqrmViola.svg';
-// import {ReactComponent as sqrmBlu} from '../../images/amenities/sqrmBlu.svg';
-// import {ReactComponent as sqrmVerde} from '../../images/amenities/sqrmVerde.svg';
+// import {ReactComponent as Sqrm20} from '../../images/amenities/sqrm20.svg';
+// import {ReactComponent as Sqrm55} from '../../images/amenities/sqrm55.svg';
 // import { Link, useLocation } from "react-router-dom";
 
-// const RoomsLinks = () => {
-//   const pathname = useLocation().pathname;
+
 
 const Amenities = () => {
- 
+
+//   const RoomsLinks = () => {
+//     const pathname = useLocation().pathname;
+
   return (
     <div className="col-start-2 col-end-9 flex flex-row justify-between space-x-2 md:col-end-8 sm2:col-start-3 sm2:justify-start sm2:space-x-5 sm3:col-start-4 sm3:col-end-10">
       <WiFi className="md:w-8 lg:w-12"/>
@@ -23,7 +23,7 @@ const Amenities = () => {
       <TV className="md:w-8 lg:w-12"/>
       <Shower className="md:w-8 lg:w-12"/>
       <HairDryer className="md:w-8 lg:w-12"/>
-      {/* if ${pathname==='/arizona'} return <sqrmArizona />; */}
+      {/* { ${pathname==='/arizona'} ? return <Sqrm20 /> } */}
     </div>
   );
 };
