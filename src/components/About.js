9276355a1b08e25e96fb19fc048f1aa3f679e3df
kostasptr <kostas-ptr@hotmail.com>
2021-10-27 19:17:04 +0300

    px changed to mx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="grid grid-cols-12 px-1 gap-x-2 pb-5 md:px-0 md:pb-0 md:mx-7 md:mb-194 lg:mx-4c lg:gap-x-4 lg:mb-294 sm2:gap-x-1 sm2:mx-10 sm2:mb-60 sm3:gap-x-8 sm3:mx-16 t sm3:mb-64">
+    <div className="grid grid-cols-12 mx-1 gap-x-2 mb-5 md:px-0 md:pb-0 md:mx-7 md:mb-194 lg:mx-4c lg:gap-x-4 lg:mb-294 sm2:gap-x-1 sm2:mx-10 sm2:mb-60 sm3:gap-x-8 sm3:mx-16 t sm3:mb-64">
 			<div className="col-start-2 col-end-12 font-sans not-italic leading-4 text-left font-light text-small12 tracking-normal md:text-xl md:leading-7 lg:text-2xl lg:leading-8 sm2:col-end-10 sm2:text-f24 sm2:leading-8 sm2:tracking-normal sm3:text-f26 sm3:leading-height36 sm3:font-ligh sm3:col-end-9">
 				<p className="font-normal text-22_green ">About:</p>
 				<p className="text-02_green md:mb-3b sm2:mb-4 sm3:mb-4b">Our goal at Mesogheo is to provide you an authentic experience of fresh tradition.<br></br>
