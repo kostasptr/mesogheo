4a300546e24ea4bb1b984141382d85ae7d7086ee
kostasptr <kostas-ptr@hotmail.com>
2021-10-27 18:23:16 +0300

    dots color fixed for 1440 & 1920 screens

--- src/components/KeenSliderDot.js
+++ src/components/KeenSliderDot.js
@@ -1,28 +1,46 @@
 import { useState } from "react";
-
 import {ReactComponent as DotActive} from "../images/carousel-navigation/dot-active.svg";
 import {ReactComponent as Dot} from "../images/carousel-navigation/dot.svg";
 
 const KeenSliderDot = ({ active, onClick }) => {
   const [isHovered, setHovered] = useState(false);
-  // let intViewportWidth = window.innerWidth;
+  let intViewportWidth = window.innerWidth;
   const textColor = isHovered ? "text-pink" : "text-22_green";
+  const textColorWide = isHovered ? "text-pink" : "text-92_green";
   const toggleHover = () => setHovered(!isHovered);
-  return active ? (
-    <DotActive
-      className={`cursor-pointer w-2.5 md:w-2a lg:w-3b ${textColor}`}
-      onClick={onClick}
-      onMouseEnter={toggleHover}
-      onMouseLeave={toggleHover}
-    />
-  ) : (
-    <Dot
-      className={`cursor-pointer w-2.5 md:w-2a lg:w-3b ${textColor}`}
-      onClick={onClick}
-      onMouseEnter={toggleHover}
-      onMouseLeave={toggleHover}
-    />
-  );
+  if (intViewportWidth<1440){
+    return active ? (
+      <DotActive
+        className={`cursor-pointer w-2.5 md:w-2a lg:w-3b ${textColor}`}
+        onClick={onClick}
+        onMouseEnter={toggleHover}
+        onMouseLeave={toggleHover}
+      />
+    ) : (
+      <Dot
+        className={`cursor-pointer w-2.5 md:w-2a lg:w-3b ${textColor}`}
+        onClick={onClick}
+        onMouseEnter={toggleHover}
+        onMouseLeave={toggleHover}
+      />
+    );
+  }
+    return active ? (
+      <DotActive
+        className={`cursor-pointer w-2.5 md:w-2a lg:w-3b ${textColorWide}`}
+        onClick={onClick}
+        onMouseEnter={toggleHover}
+        onMouseLeave={toggleHover}
+      />
+    ) : (
+      <Dot
+        className={`cursor-pointer w-2.5 md:w-2a lg:w-3b ${textColorWide}`}
+        onClick={onClick}
+        onMouseEnter={toggleHover}
+        onMouseLeave={toggleHover}
+      />
+    );
+  
 }
 
 export default KeenSliderDot;
