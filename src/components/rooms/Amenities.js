96b5d189fff0e71a0f4033d51ac9728fa512260d
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-28 03:25:57 +0200

    Added BathTub

--- src/components/rooms/Amenities.js
+++ src/components/rooms/Amenities.js
@@ -3,6 +3,7 @@ import {ReactComponent as Breakfast} from '../../images/amenities/breakfast.svg'
 import {ReactComponent as AirConditioner} from '../../images/amenities/air_conditioner.svg';
 import {ReactComponent as TV} from '../../images/amenities/tv.svg';
 import {ReactComponent as Shower} from '../../images/amenities/shower.svg';
+import {ReactComponent as BathTub} from '../../images/amenities/bath_tub.svg';
 import {ReactComponent as HairDryer} from '../../images/amenities/hair_dryer.svg';
 import {ReactComponent as Sqrm20} from '../../images/amenities/sqrm20.svg';
 import {ReactComponent as Sqrm55} from '../../images/amenities/sqrm55.svg';
@@ -19,7 +20,7 @@ const Amenities = () => {
           <Breakfast className="md:w-8 lg:w-12"/>
           <AirConditioner className="md:w-8 lg:w-12"/>
           <TV className="md:w-8 lg:w-12"/>
-          <Shower className="md:w-8 lg:w-12"/>
+          { pathname === '/arizona' ? <Shower className="md:w-8 lg:w-12"/> : <BathTub className="md:w-8 lg:w-12" /> }
           <HairDryer className="md:w-8 lg:w-12"/>
           <Sqrm20 className="md:w-8 lg:w-12"/>
         </div>
