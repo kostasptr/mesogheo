2d43e37a0d3bd167fea6a09b968315c03bdd84c4
Kostas Petrou <kostas-ptr@hotmail.com>
2021-12-07 03:42:01 +0100

    New chevrons

--- src/components/KeenSlider/index.js
+++ src/components/KeenSlider/index.js
@@ -3,8 +3,8 @@ import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { useLocation } from "react-router-dom";
 
-import ArrowLeft from "./_ArrowLeft";
-import ArrowRight from "./_ArrowRight";
+import {ReactComponent as ChevronLeft} from "../../images/carousel-navigation/chevron-left.svg";
+import {ReactComponent as ChevronRight} from "../../images/carousel-navigation/chevron-right.svg";
 import Dot from "./_Dot";
 
 import "./index.css";
@@ -47,10 +47,10 @@ const KeenSlider = ({ imagesArray, title }) => {
                 }
               )}
               { title
-                  ? (<figcaption className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-serif font-medium text-t28 md:text-f3.25 lg:text-4.25xl sm2:text-f3.625 sm3:text-f5 tracking-xlwidest md:tracking-sp20 lg:tracking-sp24 sm2:tracking-sp32 sm3:tracking-sp36 leading-tight md:leading-height67 lg:leading-height44 sm2:leading-height60 ${images[0].shadow ? 'filter drop-shadow-0.5' : ''}`}>{images[0]?.title || title}</figcaption>)
+                  ? (<figcaption className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-serif font-medium text-t28 md:text-f3.25 lg:text-4.25xl sm2:text-f3.625 sm3:text-f4.625 tracking-xlwidest md:tracking-sp20 lg:tracking-sp24 sm2:tracking-sp32 sm3:tracking-sp36 leading-tight md:leading-height67 lg:leading-height44 sm2:leading-height60 ${images[0].shadow ? 'filter drop-shadow-0.5' : ''}`}>{images[0]?.title || title}</figcaption>)
                   : (
                     <a href={images[0].title.toLowerCase()} className="absolute top-0 left-0 w-full h-full">
-                      <figcaption className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-serif font-medium text-t28 md:text-f3.25 lg:text-4.25xl sm2:text-f3.625 sm3:text-f5 tracking-xlwidest md:tracking-sp20 lg:tracking-sp24 sm2:tracking-sp32 sm3:tracking-sp36 leading-tight md:leading-height67 lg:leading-height44 sm2:leading-height60 ${images[0].shadow ? 'filter drop-shadow-0.5' : ''}`}>{images[0]?.title || title}</figcaption>
+                      <figcaption className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-serif font-medium text-t28 md:text-f3.25 lg:text-4.25xl sm2:text-f3.625 sm3:text-f4.625 tracking-xlwidest md:tracking-sp20 lg:tracking-sp24 sm2:tracking-sp32 sm3:tracking-sp36 leading-tight md:leading-height67 lg:leading-height44 sm2:leading-height60 ${images[0].shadow ? 'filter drop-shadow-0.5' : ''}`}>{images[0]?.title || title}</figcaption>
                     </a>
                     )
               }
@@ -58,17 +58,19 @@ const KeenSlider = ({ imagesArray, title }) => {
           ))}
         </div>
         {slider && (
-          <div className="hidden lg:block">
-            <ArrowLeft
+          <div className="hidden 2lg:block">
+            <ChevronLeft
+              className="absolute top-1/2 left-2 sm3:left-3a transform -translate-y-1/2 h-9 sm2:h-5e sm3:h-60"
               onClick={(e) => e.stopPropagation() || slider.prev()}
             />
-            <ArrowRight
+            <ChevronRight
+              className="absolute top-1/2 right-2 sm3:right-3a transform -translate-y-1/2 h-9 sm2:h-5e sm3:h-60"
               onClick={(e) => e.stopPropagation() || slider.next()}
             />
           </div>
         )}
         {slider && (
-          <div className="flex justify-center space-x-0.5 md:space-x-1 lg:space-x-2 absolute z-10 bottom-2 -inset-x-full h-2.5 md:h-3 lg:h-4">
+          <div className="flex justify-center space-x-0.5 md:space-x-1.5 absolute z-10 bottom-2 -inset-x-full h-2.5 md:h-3b">
             {[...Array(slider.details().size).keys()].map((idx) => {
               return <Dot
                         active={currentSlide === idx} key={idx} 
